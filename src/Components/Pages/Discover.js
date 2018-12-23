b65cdb2dc5dcbe09a0d6053ce9e4b1058b1b5d37
Mark Fisher <mfisher36@gmail.com>
2018-12-23 02:08:39 -0500

    adding work

--- src/Components/Pages/Discover.js
+++ src/Components/Pages/Discover.js
@@ -6,25 +6,22 @@ import Cards from "../Cards";
 class Discover extends Component {
 
   state = {
-      dogPhoto: "",
-      dogArray: [],
-      cardOneID: -1,
-      cardTwoID: -1,
-      cardOneKey: -1,
-      cardTwoKey: -1,
-      hiddenIDs: [],
-      message: "Pick a card!"
+    dogPhoto: "",
+    dogArray: [],
+    cardOneID: -1,
+    cardTwoID: -1,
+    cardOneKey: -1,
+    cardTwoKey: -1,
+    hiddenIDs: [],
+    message: "Pick a card!"
   }
 
   componentDidMount() {
     this.getDogs();
   }
 
-
   getDogs = () => {
     get6RandomDogs().then(({ data }) => {
-
-
 
       const dogsArray = [];
       const dogsArrayCopy = [];
@@ -42,7 +39,6 @@ class Discover extends Component {
 
       const fullDogArray = dogsArray.concat(dogsArrayCopy)
 
-
       let dogsWithKeys = fullDogArray.map((pup, i) => {
         pup.key = i
         return pup;
@@ -54,39 +50,19 @@ class Discover extends Component {
         dogArray: shuffledDogs
       })
 
-
     }).catch(err => console.log(err))
   }
 
   flipCard = (id, key) => {
-
-
-
     if (this.state.cardOneID === -1) {
-
       this.setCardOne(id, key);
-
     } else {
-
-
       (this.state.cardOneKey === key) ?
-        (this.setState({ message: "Can't click the same card!" })) :
-        (this.setCardTwo(id, key));
-
-
-
-
-
-
+        (this.setState({ message: "" })) : (this.setCardTwo(id, key));
     }
   }
 
-
   setCardOne = (id, key) => {
-
-
-
-
     this.setState({
       message: "Find the match!",
       cardOneID: id,
@@ -98,29 +74,33 @@ class Discover extends Component {
     this.setState({
       cardTwoID: id,
       cardTwoKey: key,
-  }, () => {
-    return (this.state.cardOneID === this.state.cardTwoID) ?
-    (this.resetCards("winner", id),
-      this.state.hiddenIDs.push(id)) :
-    (this.resetCards("loser"));
-  })
-}
+    }, () => {
+      return (this.state.cardOneID === this.state.cardTwoID) ?
+        (this.resetCards("winner", id)) :
+        (this.resetCards("loser"));
+    })
+  }
 
   resetCards = (result, id) => {
     this.setState({
-      message: (result === "winner") ? ("It's a match! Get another match!") : ("Not a match! Try Again!"),
-      cardOne: -1,
-      cardTwo: -1,
-      cardOneKey: -1,
-      cardTwoKey: -1,
-    
-
+      message: (result === "winner") ?
+        ("It's a match! Get another match!") : ("Not a match! Try Again!")
+    }, () => {
+      setTimeout(() => {
+        if (result === "winner") {
+          this.state.hiddenIDs.push(id)
+        }
+        this.setState({
+          cardOneID: -1,
+          cardTwoID: -1,
+          cardOneKey: -1,
+          cardTwoKey: -1,
+        })
+      }, 1000)
     })
-
   }
 
   shuffleCards = (array) => {
-
     let currentIndex = array.length, temporaryValue, randomIndex;
     while (0 !== currentIndex) {
       randomIndex = Math.floor(Math.random() * currentIndex);
@@ -133,20 +113,24 @@ class Discover extends Component {
   }
 
 
-
-
   render() {
 
-    console.log(this.props)
-    console.log(this.state)
-
-
     return (
-      <div className="container">
-        <h1>{this.state.message}</h1>
+
+      <div className="container text-center">
+
+        {<h1 className="m-2">
+          {(this.state.dogArray.length / 2 === this.state.hiddenIDs.length) ?
+            (`You win!`) :((this.state.message === "") ?
+              ('\xa0') :(this.state.message))}
+        </h1>}
+
         <div className="row">
+
           {this.state.dogArray.map(pup => {
+
             return (
+
               <Cards
                 id={pup.id}
                 key={pup.key}
@@ -160,9 +144,11 @@ class Discover extends Component {
                     ? ("blank") : ("dogImage")
                 }
               />
+
             )
 
           })}
+
         </div>
       </div>
     )
